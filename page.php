3a9c2be28aa6216de140e6b7b4c9e9e0478206a4
Claudius Sachinda <csachinda@gmail.com>
2026-02-17 18:50:50 +0200

    hide quote price for logged out users

--- page.php
+++ page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -13,31 +14,37 @@
  */
 
 // If this is a WooCommerce page (Cart, Checkout, My Account), use singular template.
-if ( function_exists( 'is_cart' ) && ( is_cart() || is_checkout() || is_account_page() ) ) {
-	get_template_part( 'singular' );
+if (function_exists('is_cart') && (is_cart() || is_checkout() || is_account_page())) {
+	get_template_part('singular');
+	return;
+}
+
+// If this is the Request a Quote page, use singular template (no sidebar).
+if (is_page('request-a-quote')) {
+	get_template_part('singular');
 	return;
 }
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+	<?php
+	while (have_posts()) :
+		the_post();
 
-			get_template_part( 'template-parts/content', 'page' );
+		get_template_part('template-parts/content', 'page');
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if (comments_open() || get_comments_number()) :
+			comments_template();
+		endif;
 
-		endwhile; // End of the loop.
-		?>
+	endwhile; // End of the loop.
+	?>
 
-	</main><!-- #main -->
+</main><!-- #main -->
 
 <?php
 get_sidebar();
